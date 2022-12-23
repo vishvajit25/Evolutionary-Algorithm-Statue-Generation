e1cdb2bc1035468e2028ae79b179b45fc91799d0
imvee <vind.veeru@gmail.com>
2022-12-24 00:10:51 +0530

    Fitness Function

--- CGen.py
+++ CGen.py
@@ -18,7 +18,7 @@ def generate_vertices(L,B,H,seed):
     volume=L*B*H
     
     #generate random number of vertices (min-3, max-volume of bounding frame)
-    numOfVertices=random.randint(3,volume)
+    numOfVertices=random.randint(3,int(volume**(1/3)))
     
     #generate vertices array
     VERTICES=[]
